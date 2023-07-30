b71c1823eee545868e36811d842b87be6118736c
Johannes Löthberg <johannes@kyriasis.com>
2023-07-30 20:53:53 +0200

    Add NORWEGIAN_SE_NORWNOK1 bank
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- src/app-gocardless/bank-factory.js
+++ src/app-gocardless/bank-factory.js
@@ -1,9 +1,15 @@
 import IngPlIngbplpw from './banks/ing-pl-ingbplpw.js';
 import IntegrationBank from './banks/integration-bank.js';
 import MbankRetailBrexplpw from './banks/mbank-retail-brexplpw.js';
+import NorwegianXxNorwnok1 from './banks/norwegian-xx-norwnok1.js';
 import SandboxfinanceSfin0000 from './banks/sandboxfinance-sfin0000.js';
 
-const banks = [MbankRetailBrexplpw, SandboxfinanceSfin0000, IngPlIngbplpw];
+const banks = [
+  IngPlIngbplpw,
+  MbankRetailBrexplpw,
+  NorwegianXxNorwnok1,
+  SandboxfinanceSfin0000,
+];
 
 export default (institutionId) =>
   banks.find((b) => b.institutionIds.includes(institutionId)) ||
